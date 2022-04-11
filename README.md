5fa8abc7415bb845fbb927155caab217f96ae1a2
Jimmy Mugisha <mugishajmmy@gmail.com>
2022-04-11 16:16:27 +0300

    Third Commit - implemented the function bodies and modified the README.md file

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 # Author: Jimmy Mugisha
 
 ## Project overview
-This is a c programaimed at providing statistical analytics on a given dataset.
+This is a c program aimed at providing statistical analytics on a given dataset.
 The input dataset is an N-sized one-dimensional array
--- stats.c
+++ stats.c
@@ -38,48 +38,108 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  // Sorting the given array
+  sort_array(test, SIZE);
+
+  //printing the statistics
+  print_statistics(test, SIZE);
+
+  // Printing the sorted array
+  printf("\nSorted array: \n");
+  print_array(test, SIZE);
+
 }
 
 /* Add other Implementation File Code Here */
 
 void print_statistics(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  printf("The median is: %f \n", find_median(array, array_length));
+  printf("The mean is: %d \n", find_mean(array, array_length));
+  printf("The maximum is: %d \n", find_maximum(array, array_length));
+  printf("The minimum is: %d \n", find_minimum(array, array_length));
 }
 
 void print_array(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  int i; 
+  for (i=0; i < array_length; i++) 
+  printf("%d ", array[i]); 
+  printf("\n"); 
 }
 
 float find_median(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  float median = 0.0;
+  if (array_length % 2 == 0)
+  {
+    // Type casting the unsigned char to float
+    median = (float)(array[array_length/2 - 1] + array[array_length/2])/2.0;
+  }
+  else
+  {
+    median = array[array_length/2 + 1];
+  }
+
+  return median;
+  
 }
 
 float find_mean(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  int sum = 0;
+  for(int i=0; i<array_length; i++)
+  {
+    sum += array[i];
+  }
+
+  return sum/array_length;
 }
 
 unsigned char find_maximum(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  unsigned char max_value = array[0];
+  for(int i=0; i<array_length; i++)
+  {
+    if (array[i] > max_value)
+    {
+      max_value = array[i];
+    }
+    
+  }
+  return max_value;
+
 }
 
 unsigned char find_minimum(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  unsigned char min_value = array[0];
+  for(int i=0; i<array_length; i++)
+  {
+    if (array[i] < min_value)
+    {
+      min_value = array[i];
+    }
+    
+  }
+  return min_value;
+
 }
 
 void sort_array(unsigned char array[], unsigned int array_length)
 {
-  // Body
+  // using the bubble sort algorithm
+  int i, j; 
+  for (i = 0; i < array_length-1; i++)       
+  for (j = 0; j < array_length-i-1; j++) if (array[j] < array[j+1]) 
+  swap(&array[j], &array[j+1]);
 }
 
 // A helper function to swap elements 
 void swap(unsigned char *a, unsigned char *b) 
 { 
-  // Body 
+  unsigned char temp = *a; 
+  *a = *b; 
+  *b = temp; 
 }  
 
